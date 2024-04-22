237ec06c4380cec2a37f475c9f49013a68a5ba48
Marcos Souza <marcostullio.s@gmail.com>
2024-04-22 14:10:06 -0300

    alterado o desing do login

--- callsystem.php
+++ callsystem.php
@@ -79,14 +79,14 @@
 							</select>
 						</div>
 					</div>
-					<!-- adicionado campo do local do chamado -->
+					<!-- adicionado campo do local do chamado
 					<div class="callsystem_form_fields">
 						<div class="div_label">Local: </div>
 						<div class="div_form_field">
 							<input type="hidden" id="product_id" name="product_id">
 							<input name="product_field" id="product_field">
 						</div>
-					</div>
+					</div> -->
 					<div class="callsystem_form_fields">
 						<div class="div_label">Cliente: </div>
 						<div class="div_form_field">
--- index.php
+++ index.php
@@ -1,48 +1,52 @@
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="jqueryUI/jquery-ui.min.css">
-	<link rel="stylesheet" href="css/extra-style.css">
-	<script src="external/jquery/jquery.js"></script>
-	<script src="jslibs/js.cookie.js"></script>
-	<script src="jqueryUI/jquery-ui.min.js"></script>	
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="jqueryUI/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/extra-style.css">
+    <script src="external/jquery/jquery.js"></script>
+    <script src="jslibs/js.cookie.js"></script>
+    <script src="jqueryUI/jquery-ui.min.js"></script>    
 </head>
 <body>
-	<div>
-	<div id="div_login" class="ui-widget ui-front ui-widget-content ui-corner-all ui-widget-shadow">
-		<div class="login_fields">
-			<div id="div_user">Usuário:</div><div id="user_field"><input id="username" size="25"></div>
-		</div>
-		<div class="login_fields">
-			<div id="div_pass">Senha:</div><div id="pass_field"><input type="password" id="password" size="25"></div>
-		</div>
-		<div class="login_fields" id="div_btn">
-			<button id="btn_login">Entrar</button>
-		</div>
+    <div class="logo-hi-mix">
+        <img src="/css/images/Logo-White.svg" height="100px" width="200px">
+		<p>Chamados Engenharia de Teste</p>
 	</div>
-	<div id="alert_empty_field" class="ui-widget" style="display: none">
-		<div class="ui-state-error ui-corner-all">
-			<p><span class="ui-icon ui-icon-alert"></span>
-			<strong>Atenção:</strong> Campo de usuário ou senha vazio.</p>
-		</div>
-	</div>
-	<div id="alert_wrong_user" class="ui-widget" style="display: none">
-		<div class="ui-state-error ui-corner-all">
-			<p><span class="ui-icon ui-icon-alert"></span>
-			<strong>Atenção:</strong> Usuário ou senha incorreto.</p>
-		</div>
-	</div>
-	<div id="alert_blocked_user" class="ui-widget" style="display: none">
-		<div class="ui-state-error ui-corner-all">
-			<p><span class="ui-icon ui-icon-alert"></span>
-			<strong>Atenção:</strong> Usuário não possui permissão para abrir chamados.</p>
-		</div>
-	</div>
-	</div>
-	
-	<script src="jslibs/functions.js"></script>
-	
+    <div>
+    <div id="div_login" class="ui-widget ui-front ui-widget-content ui-corner-all ui-widget-shadow">
+        <div class="login_fields">
+            <div id="div_user">Usuário:</div><div id="user_field"><input id="username" size="25"></div>
+        </div>
+        <div class="login_fields">
+            <div id="div_pass">Senha:</div><div id="pass_field"><input type="password" id="password" size="25"></div>
+        </div>
+        <div class="login_fields" id="div_btn">
+            <button id="btn_login">Entrar</button>
+        </div>
+    </div>
+    <div id="alert_empty_field" class="ui-widget" style="display: none">
+        <div class="ui-state-error ui-corner-all">
+            <p><span class="ui-icon ui-icon-alert"></span>
+            <strong>Atenção:</strong> Campo de usuário ou senha vazio.</p>
+        </div>
+    </div>
+    <div id="alert_wrong_user" class="ui-widget" style="display: none">
+        <div class="ui-state-error ui-corner-all">
+            <p><span class="ui-icon ui-icon-alert"></span>
+            <strong>Atenção:</strong> Usuário ou senha incorreto.</p>
+        </div>
+    </div>
+    <div id="alert_blocked_user" class="ui-widget" style="display: none">
+        <div class="ui-state-error ui-corner-all">
+            <p><span class="ui-icon ui-icon-alert"></span>
+            <strong>Atenção:</strong> Usuário não possui permissão para abrir chamados.</p>
+        </div>
+    </div>
+    </div>
+    
+    <script src="jslibs/functions.js"></script>
+    
 </body>
 </html>
